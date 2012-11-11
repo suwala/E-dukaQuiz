435957c812215c2ad369e68b006154e8cc26bdda
suwala <aoisaoais@gmail.com>
2012-11-11 16:35:37 +0900

    mosaic kai

--- src/sample/edukaquizMoza/Mosaic_image.java
+++ src/sample/edukaquizMoza/Mosaic_image.java
@@ -13,8 +13,8 @@ public class Mosaic_image{
 
 		dot = dot == 0?1:dot;
 		
-		for (int i = 0; i < b.getWidth() / dot; i++) {
-			for (int j = 0; j < b.getHeight() / dot; j++) {
+		for (int i = 0; i < b.getWidth() / dot+1; i++) {
+			for (int j = 0; j < b.getHeight() / dot+1; j++) {
 				int color = image.getPixel(i, j);
 
 				// �h�b�g�̒��̕��ϒl���g��
@@ -22,21 +22,33 @@ public class Mosaic_image{
 				int gg = 0;
 				int bb = 0;
 				for (int k = 0; k < dot; k++) {
+					if(i*dot+k >= b.getWidth())
+						break;	
 					for (int l = 0; l < dot; l++) {
+						
+						if(j*dot+l >= b.getHeight())
+							break;
+										
 						int dotColor = image.getPixel(i * dot + k, j * dot + l);
 						rr += Color.red(dotColor);
 						gg += Color.green(dotColor);
 						bb += Color.blue(dotColor);
 					}
+						
 				}
 				rr = rr / (dot * dot);
 				gg = gg / (dot * dot);
 				bb = bb / (dot * dot);
 
 				for (int k = 0; k < dot; k++) {
+					if(i*dot+k >= b.getWidth())
+						break;	
 					for (int l = 0; l < dot; l++) {
+						if(j*dot+l >= b.getHeight())
+							break;
 						b.setPixel(i * dot + k, j * dot + l, Color.rgb(rr, gg, bb));
 					}
+						
 				}
 			}
 		}
