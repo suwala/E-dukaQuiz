abca2aac8f1eb501e0c7870421d741f89cf8fd63
suwala <aoisaoais@gmail.com>
2012-11-19 17:00:27 +0900

    11/19home

--- gen/sample/edukaquizMoza/R.java
+++ gen/sample/edukaquizMoza/R.java
@@ -51,8 +51,9 @@ public final class R {
         public static final int judge=0x7f030001;
         public static final int menu=0x7f030002;
         public static final int oauthl=0x7f030003;
-        public static final int question=0x7f030004;
-        public static final int result=0x7f030005;
+        public static final int postquiz=0x7f030004;
+        public static final int question=0x7f030005;
+        public static final int result=0x7f030006;
     }
     public static final class menu {
         public static final int activity_main=0x7f070000;
